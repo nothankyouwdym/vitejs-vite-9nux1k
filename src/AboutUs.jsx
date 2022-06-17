37187689bc07ffa62225446eae78afb9a53e0259
nothankyouwdym <boubacar.diallo2028@uppublicschools.org>
2022-06-16 20:20:48 -0400

    1 file modified

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -10,7 +10,7 @@ class AboutUs extends Component{
           <p>
             <strong>
               <em>
-                Hey, welcome to the project since you clicked to learn about us um luan made the notes and told me to put some of the text in I, Boubacar made the code and the website. It was fun and took like around half a day.
+                Hey, welcome to the project since you clicked to learn about us um luan made the notes and told me to put some of the text in I, Boubacar made the code and the website. It was fun and took like around 4 hours and 50 minutes.
               </em>
             </strong>
           </p>
