9d00898ad0a3855a4c1f0e3597e037dad21e8a19
nothankyouwdym <boubacar.diallo2028@uppublicschools.org>
2022-06-21 12:45:22 -0400

    1 file modified

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,6 +8,7 @@ class Home extends Component{
         <strong>
           <em>
             Hi, this is Boubacar's and Luan's project welcome to our thermal energy page.
+            <img></img>
           </em>
         </strong>
       </p>
