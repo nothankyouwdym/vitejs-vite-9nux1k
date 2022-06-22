061a490a0781be3a3e6601e44683b25c2b0f9a5a
nothankyouwdym <boubacar.diallo2028@uppublicschools.org>
2022-06-22 09:10:10 -0400

    1 file modified, 1 deleted

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,8 +10,7 @@ class Home extends Component{
             Hi, this is Boubacar's and Luan's project welcome to our thermal energy page. Click the links to navigate around the website. Click the energy link to enter our second project and if you go there you can click the nukes link to send you back to nukes.
           </em>
         </strong>
-      </p>,
-      <img src="./Nuke" alt="Explosion"></img>
+      </p>
     );
   }
 }
